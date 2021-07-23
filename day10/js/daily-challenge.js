438de367b37df5af2f006067ca5c2901ef03375d
Creature <Creature.Creations@outlook.com>
2021-07-22 22:29:20 -0700

    completed daily challenge

--- day10/js/daily-challenge.js
+++ day10/js/daily-challenge.js
@@ -1,6 +1,3 @@
-// WIP
-
-
 // -- get variables --
 const user = document.getElementById('username');
 const pw = document.getElementById('password');
@@ -8,7 +5,7 @@ const repeatPW = document.getElementById('repeatPassword');
 const submitButton = document.getElementById('submit');
 // -- end variables --
 
-
+// -- functions --
 // create function to check if submit button should be disabled or not
 function btnCheck() {
     if (user.value == '' || pw.value == '' || repeatPW.value == ''){ 
@@ -19,7 +16,28 @@ function btnCheck() {
     }
 }
 
+function pwCheck(pw, repeatPw){
+    if (pw == repeatPw){
+        alert('Registration successful.')
+    }
+    else{
+        alert('Your passwords do not match.')
+    }
+
+}
+
+// -- end functions --
+
 // add event listeners
-user.addEventListener('input', btnCheck());
-pw.addEventListener('keypress', btnCheck());
-repeatPW.addEventListener('keyup', btnCheck());
+user.addEventListener('input', btnCheck);
+pw.addEventListener('keypress', btnCheck);
+repeatPW.addEventListener('keyup', btnCheck);
+submitButton.addEventListener('click', () => {
+    if (pw.value == repeatPW.value){
+        alert('Registration successful.')
+    }
+    else{
+        alert('Your passwords do not match.')
+    }
+
+});
