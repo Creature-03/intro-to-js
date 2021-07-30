34c4df2ebe59fb6caabb4a1e6ec6050bd2ce366f
Creature <Creature.Creations@outlook.com>
2021-07-30 11:30:31 -0700

    added core review outline to main.js

---
+++ test/js/main.js
@@ -0,0 +1,153 @@
+// --- Data types ---
+
+    // Integers(numbers)
+
+
+    // Strings(text)
+
+
+    // Booleans(True/False)
+
+
+    // Undefined(nothing)
+
+
+
+// --- Variables ---
+
+    // const(constant)
+
+    
+    // let(changable)
+
+
+
+// --- Operators ---
+
+    // = (assignment)
+
+
+    // += (addtion assignment)
+
+
+    // -= (subtraction assignment)
+
+
+    // *= (multiplication assignment)
+
+
+    // /= (division assignment)
+
+    
+    // +, -, *, /, % (math)
+
+
+
+// --- popup methods ---
+
+    // alert (tell the user something)
+
+
+    // confirm (ask the user to confirm true or false)
+
+
+    // prompt (ask the user for an input)
+
+
+
+// --- if/else statements ---
+
+
+
+// --- comparison operators ---
+
+    // == (equal to)
+
+
+    // === (strict equal to)
+
+
+    // != (not equal to)
+
+
+    // !== (strict not equal to)
+
+    
+    // > (greater than)
+
+
+    // < (less than)
+
+
+    // >= (greater than or equal to)
+
+
+    // <= (less than or equal to)
+
+
+// --- Truthy and Falsy ---
+
+    // falsy
+
+
+    // truthy
+
+
+
+// --- logical operators ---
+
+    // && (and)
+
+    
+    // || (or)
+
+
+    // ! (not)
+
+
+
+// --- functions ---
+
+    // function
+
+
+    // arrow function
+
+    
+    // return
+
+
+
+// --- methods ---
+
+
+
+// --- arrays ---
+
+    // arrays
+
+
+    // index
+
+
+
+// --- loops ---
+
+    // for loops
+
+
+    // while loops
+
+
+
+// --- objects/dictionaries ---
+
+
+
+// --- classes ---
+
+
+
+
+// --- chaining functions and methods ---
+
